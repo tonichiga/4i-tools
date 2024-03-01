5c3d16e2c508f1540e2a038499709b4cbb21b4c2
Toni <tonichiga@gmail.com>
2024-03-01 10:28:37 +0200

    added export

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.constants = exports.detectPlatform = exports.detectBrowser = exports.iOS = exports.validations = void 0;
+exports.tools = exports.constants = exports.detectPlatform = exports.detectBrowser = exports.iOS = exports.validations = void 0;
 require("./hooks");
 require("./components");
 var validations_1 = require("./shared/validations");
@@ -16,3 +16,5 @@ var detect_platform_2 = require("./shared/detect-platform");
 Object.defineProperty(exports, "detectPlatform", { enumerable: true, get: function () { return __importDefault(detect_platform_2).default; } });
 var constants_1 = require("./shared/constants");
 Object.defineProperty(exports, "constants", { enumerable: true, get: function () { return __importDefault(constants_1).default; } });
+var tools_1 = require("./tools");
+Object.defineProperty(exports, "tools", { enumerable: true, get: function () { return __importDefault(tools_1).default; } });
--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,4 @@ export { default as iOS } from "./shared/detect-platform";
 export { default as detectBrowser } from "./shared/detect-browser";
 export { default as detectPlatform } from "./shared/detect-platform";
 export { default as constants } from "./shared/constants";
+export { default as tools } from "./tools";
